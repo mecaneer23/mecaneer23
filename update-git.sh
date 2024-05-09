95d355c86f1fe811b6237fa8a9bda0ca0b14be0f
Gabriel Natenshon <mecaneer23@gmail.com>
2024-05-08 22:18:33 -0500

    feat: revert echo and fix git getting excited because of no quotes

--- update-git.sh
+++ update-git.sh
@@ -2,9 +2,10 @@
 
 commit_message="Update recent commit SVG from $(cat prev-file-name.txt) to $(cat current-file-name.txt)"
 echo $(cat current-file-name.txt) > prev-file-name.txt
+echo $commit_message
 
 git config --global user.name 'GitHub Actions'
 git config --global user.email 'actions@github.com'
 git add README.md recent-commit.svg
-git diff --quiet && git diff --staged --quiet || git commit -m $commit_message
+git diff --quiet && git diff --staged --quiet || git commit -m "$commit_message"
 git push
