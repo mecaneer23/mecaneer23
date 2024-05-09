5d5a7aa2b487b222450415f869905c396e6daabd
Gabriel Natenshon <mecaneer23@gmail.com>
2024-05-08 22:07:11 -0500

    feat: don't log more than necessary

--- update-git.sh
+++ update-git.sh
@@ -2,7 +2,6 @@
 
 commit_message="Update recent commit SVG from $(cat prev-file-name.txt) to $(cat current-file-name.txt)"
 echo $(cat current-file-name.txt) > prev-file-name.txt
-echo $commit_message
 
 git config --global user.name 'GitHub Actions'
 git config --global user.email 'actions@github.com'
