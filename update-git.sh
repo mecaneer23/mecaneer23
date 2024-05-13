48198b346a7b5c68aece819637c2cc0d592b4018
Gabriel Natenshon <mecaneer23@gmail.com>
2024-05-13 14:31:48 -0500

    feat: add update function which is called conditionally

--- update-git.sh
+++ update-git.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# [ -f current-file-name.txt ] || echo "Repo hasn't changed"
+update() {
+  commit_message="Update recent commit SVG from $(cat prev-file-name.txt) to $(cat current-file-name.txt)"
+  echo $(cat current-file-name.txt) > prev-file-name.txt
+  echo $commit_message
+  
+  git config --global user.name 'GitHub Actions'
+  git config --global user.email 'actions@github.com'
+  git add README.md recent-commit.svg prev-file-name.txt
+  git diff --staged --quiet recent-commit.svg || git commit -m "$commit_message"
+  git push
+}
 
-commit_message="Update recent commit SVG from $(cat prev-file-name.txt) to $(cat current-file-name.txt)"
-echo $(cat current-file-name.txt) > prev-file-name.txt
-echo $commit_message
-
-git config --global user.name 'GitHub Actions'
-git config --global user.email 'actions@github.com'
-git add README.md recent-commit.svg prev-file-name.txt
-git diff --staged --quiet recent-commit.svg || git commit -m "$commit_message"
-git push
+if [ -f current-file-name.txt ]; then
+  update();
+else
+  echo "Repo hasn't changed";
+fi;
